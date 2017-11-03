a78761d034de0ff37600ab7c2ff660ea953c629e
Renee Campanilla <reneecampanilla@gmail.com>
2017-11-03 23:08:27 +0800

    implemented boilerplate

---
+++ includes/class-simple-fieldset-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       www.reneecampanilla.com
+ * @since      1.0.0
+ *
+ * @package    Simple_Fieldset
+ * @subpackage Simple_Fieldset/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Simple_Fieldset
+ * @subpackage Simple_Fieldset/includes
+ * @author     Renee Campanilla <renee.campanilla@gmail.com>
+ */
+class Simple_Fieldset_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-simple-fieldset-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       www.reneecampanilla.com
+ * @since      1.0.0
+ *
+ * @package    Simple_Fieldset
+ * @subpackage Simple_Fieldset/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Simple_Fieldset
+ * @subpackage Simple_Fieldset/includes
+ * @author     Renee Campanilla <renee.campanilla@gmail.com>
+ */
+class Simple_Fieldset_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
